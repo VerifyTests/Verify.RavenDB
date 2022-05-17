8ade09ebca7ba14c36652377b9d810f292e8c423
Simon Cropp <simon.cropp@gmail.com>
2022-05-17 22:21:36 +1000

    refs

--- src/Verify.RavenDB/VerifyRavenDB.cs
+++ src/Verify.RavenDB/VerifyRavenDB.cs
@@ -3,13 +3,11 @@
 public static class VerifyRavenDB
 {
     public static void Enable() =>
-        VerifierSettings.ModifySerialization(settings =>
-        {
-            settings.AddExtraSettings(serializerSettings =>
+        VerifierSettings
+            .AddExtraSettings(_ =>
             {
-                var converters = serializerSettings.Converters;
+                var converters = _.Converters;
                 converters.Add(new SessionConverter());
                 converters.Add(new LazyStringValueConverter());
             });
-        });
 }
